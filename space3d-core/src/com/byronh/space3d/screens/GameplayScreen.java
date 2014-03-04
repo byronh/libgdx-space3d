b17b1dc90eea46d4d3b8a5556636c964b7c7b8d9
Byron <byronh@gmail.com>
2014-03-04 08:04:57 -0800

    Added scene2d.ui skins

--- space3d-core/src/com/byronh/space3d/screens/GameplayScreen.java
+++ space3d-core/src/com/byronh/space3d/screens/GameplayScreen.java
@@ -1,6 +1,7 @@
 package com.byronh.space3d.screens;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
@@ -14,8 +15,12 @@ import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
 import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
 import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
 import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.byronh.space3d.Space3DGame;
 
 
@@ -26,25 +31,33 @@ public class GameplayScreen extends AbstractScreen {
 	Stage stage;
 
 	Image image;
+	
+	private InputMultiplexer inputMultiplexer;
 
-	public PerspectiveCamera cam;
+	private PerspectiveCamera cam;
 
-	public CameraInputController camController;
+	private CameraInputController camController;
 
-	public Environment environment;
+	private Environment environment;
 
-	public ModelBatch modelBatch;
+	private ModelBatch modelBatch;
 
-	public Model sphere;
+	private Model sphere;
 
-	public ModelInstance planet;
+	private ModelInstance planet;
+	
+	private Skin skin;
 
 	public GameplayScreen(Space3DGame game) {
 
 		this.game = game;
+		
+		inputMultiplexer = new InputMultiplexer();
 
 		modelBatch = new ModelBatch();
-
+		
+		skin = game.manager.get("ui/Holo-dark-hdpi.json", Skin.class);
+		
 		stage = new Stage();
 		image = new Image(game.manager.get("texture-maps/starscape.png", Texture.class));
 		//stage.addActor(image);
@@ -62,18 +75,33 @@ public class GameplayScreen extends AbstractScreen {
 
 		camController = new CameraInputController(cam);
 		camController.scrollFactor = -0.05f;
-		Gdx.input.setInputProcessor(camController);
 
 		ModelBuilder modelBuilder = new ModelBuilder();
 
 		sphere = modelBuilder.createSphere(2f, 2f, 2f, 40, 40, new Material(), Usage.Position | Usage.Normal | Usage.TextureCoordinates);
 		planet = new ModelInstance(sphere);
+		
+		final TextButton button = new TextButton("Click me", skin, "default");
+        button.setPosition(100, 100);
+        
+        button.addListener(new ClickListener(){
+            @Override 
+            public void clicked(InputEvent event, float x, float y){
+                button.setText("Clicked!");
+            }
+        });
+        
+        stage.addActor(button);
+        
+        inputMultiplexer.addProcessor(stage);
+        inputMultiplexer.addProcessor(camController);
+        Gdx.input.setInputProcessor(inputMultiplexer);
 	}
 
 	@Override
 	public void render(float delta) {
 
-		Gdx.gl.glClearColor(0, 0, 0, 0);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT
 				| (Gdx.graphics.getBufferFormat().coverageSampling ? GL20.GL_COVERAGE_BUFFER_BIT_NV : 0));
 
@@ -112,6 +140,6 @@ public class GameplayScreen extends AbstractScreen {
 		modelBatch.dispose();
 		sphere.dispose();
 		stage.dispose();
-		game.manager.unload("texture-maps/starscape.png");
+//		game.manager.unload("texture-maps/starscape.png");
 	}
 }
--- space3d-core/src/com/byronh/space3d/screens/LoadingScreen.java
+++ space3d-core/src/com/byronh/space3d/screens/LoadingScreen.java
@@ -3,8 +3,10 @@ package com.byronh.space3d.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.byronh.space3d.Space3DGame;
 
 
@@ -28,6 +30,7 @@ public class LoadingScreen extends AbstractScreen {
         
         // Load relevant assets for the next game screen here
         game.manager.load("texture-maps/starscape.png", Texture.class);
+        game.manager.load("ui/Holo-dark-hdpi.json", Skin.class);
     }
 
     @Override
